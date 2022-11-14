9809df7bb2e59cffb75d3aa6ef6ccefac9ff78fd
jose carlos narciso <josecdia@hotmail.com>
2022-11-14 19:03:31 -0300

    Aulas sobre Pilhas

--- src/main/java/com/estruturasDadosPilhas/exercicios/Exercicio09.java
+++ src/main/java/com/estruturasDadosPilhas/exercicios/Exercicio09.java
@@ -2,43 +2,38 @@ package com.estruturasDadosPilhas.exercicios;
 
 import java.util.Stack;
 
-public class Exercicio08 {
+public class Exercicio09 {
     public static void main( String[] args ) {
 
         Stack<Integer> original = new Stack<>();
         Stack<Integer> destino = new Stack<>();
         Stack<Integer> auxiliar = new Stack<>();
 
-        original.push(10);
-        original.push(9);
-        original.push(8);
-        original.push(7);
-        original.push(6);
-        original.push(5);
-        original.push(4);
         original.push(3);
         original.push(2);
         original.push(1);
+
         torreHanoi(original.size(),original,destino,auxiliar);
     }
 
-    public static void torreHanoi( int n, Stack<Integer> original,
-                                   Stack<Integer> destino,
-                                   Stack<Integer> auxiliar ) {
+    public static void torreHanoi( int n, Stack<Integer> original
+            , Stack<Integer> destino
+            , Stack<Integer> auxiliar ) {
 
-        if (n > 0) {
+        // Algoritmo recursivo, enquanto o n for maior que zero ele será executado.
+        // Por ser recursivo ele precisa chamar ele mesmo
+        if (n > 0){
             torreHanoi(n-1,original,auxiliar,destino);
-
             destino.push(original.pop());
 
-            System.out.println("---------");
+            System.out.println("---------- \n" + destino.size());
 
             System.out.println("Original: " + original);
             System.out.println("Destino: " + destino);
             System.out.println("Auxiliar: " + auxiliar);
-            System.out.println("---------");
-            torreHanoi(n-1,auxiliar,destino,original);
-        }
 
+            torreHanoi(n-1,auxiliar,destino,original);
+
+        }
     }
 }
